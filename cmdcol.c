653979d5b3543b108260b808f4cd0ddac07508c8
Hazel <dunn0441@umn.edu>
2021-02-15 20:23:17 -0600

    Hazel cmdcol

--- cmdcol.c
+++ cmdcol.c
@@ -3,20 +3,41 @@
 #include "commando.h"
 
 void cmdcol_add(cmdcol_t *col, cmd_t *cmd){
-    if(col->cmd[MAX_CMDS] <= col->size){ //cant compare pointers and ints - needs to be fixed
-        printf("Error, max commands exceeded");
-    }
     col->cmd[col->size] = cmd;
     col->size++;
+
+    if (col->size > MAX_CMDS){ //can we do this after? or should it be before
+        printf("Error, max commands exceeded");
+    }
+    
 }
 // Add the given cmd to the col structure. Update the cmd[] array and
 // size field. Report an error if adding would cause size to exceed
 // MAX_CMDS, the maximum number commands supported.
 
-void cmdcol_print(cmdcol_t *col);
+void cmdcol_print(cmdcol_t *col){
 // Print all cmd elements in the given col structure.  The format of
 // the table is
-//
+
+    printf("%-4s ", "JOB");
+    printf("#%-8s ", "PID");
+    printf("%4s ", "STAT");
+    printf("%10s ", "STR_STAT");
+    printf("%4s ", "OUTB");
+    printf("%-s\n", "COMMAND");
+
+    for(int i = 0; i < col->size; i++) {
+        printf("%-4d ", i);
+        printf("#%-8d ", (col->cmd[i])->pid);
+        printf("%4d ", (col->cmd[i])->status);
+        printf("%10s ", (col->cmd[i])->str_status);
+        printf("%4d ", (col->cmd[i])->output_size);
+        for (int j = 0; j < (sizeof(col->cmd[i])->argv); j++) {
+            printf("%-s ", (col->cmd[i])->argv[j]);
+        }
+        printf("\n");
+    }
+}
 // JOB  #PID      STAT   STR_STAT OUTB COMMAND
 // 0    #17434       0    EXIT(0) 2239 ls -l -a -F 
 // 1    #17435       0    EXIT(0) 3936 gcc --help 
@@ -39,9 +60,17 @@ void cmdcol_print(cmdcol_t *col);
 // left  left    right      right rigt left
 // int   int       int     string  int string
 
-void cmdcol_update_state(cmdcol_t *col, int block);
+void cmdcol_update_state(cmdcol_t *col, int block){
+    for (int i = 0; i < col->size; i++) {
+        cmd_update_state(col->cmd[i], block); 
+    }
+}
 // Update each cmd in col by calling cmd_update_state() which is also
 // passed the block argument (either NOBLOCK or DOBLOCK) 
 
-void cmdcol_freeall(cmdcol_t *col);
+void cmdcol_freeall(cmdcol_t *col){
+    for (int i = 0; i < col->size; i++) {
+        cmd_free(col->cmd[i]);
+    }
+}
 // Call cmd_free() on all of the constituent cmd_t's.
