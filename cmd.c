dc0cddf27320c40411bc4cb344289a37f67f84a5
Anjali Oleksy <anjalio235@gmail.com>
2021-02-22 21:43:51 -0600

    update

--- cmd.c
+++ cmd.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> //don't know if we need this
+#include <stdlib.h> 
 #include "commando.h"
 
 cmd_t *cmd_new(char *argv[]){
@@ -22,10 +22,10 @@ cmd_t *cmd_new(char *argv[]){
         *cmd->name = '\0';
     }
     else {
-        strcpy(cmd->name,argv[0]); // Take name of command input in argv and assign to name
+        strncpy(cmd->name,argv[0],NAME_MAX); // Take name of command input in argv and assign to name
     }
 
-    //strcpy(cmd->name,argv[0]); 
+ 
     cmd->finished = 0;            // Initialize all fields of new cmd
     snprintf(cmd->str_status, STATUS_LEN, "INIT");  
     cmd->status = -1;
@@ -38,14 +38,7 @@ cmd_t *cmd_new(char *argv[]){
     return cmd;
     cmd_free(cmd); // Free the cmd structure
 
-    
-// Allocates a new cmd_t with the given argv[] array. Makes string
-// copies of each ogf the strings contained within argv[] using
-// strdup() as they likely come from a source that will be
-// altered. Ensures that cmd->argv[] is ended with NULL. Sets the name
-// field to be the argv[0]. Sets finished to 0 (not finished yet). Set
-// str_status to be "INIT" using snprintf(). Initializes the remaining
-// fields to obvious default values such as -1s, and NULLs.
+
 }
 
 void cmd_free(cmd_t *cmd){
@@ -61,9 +54,7 @@ void cmd_free(cmd_t *cmd){
     }
 
     free(cmd);
-// Deallocates a cmd structure. Deallocates the strings in the argv[]
-// array. Also deallocats the output buffer if it is not
-// NULL. Finally, deallocates cmd itself.
+
 }
 
 void cmd_start(cmd_t *cmd){
@@ -87,14 +78,7 @@ void cmd_start(cmd_t *cmd){
         cmd->pid = child_pid;       // Assign the child pid to the parent
         close(cmd->out_pipe[PWRITE]); // Close write end of pipe
     }
-    //close(cmd->out_pipe[PREAD]);
-// Forks a process and executes command in cmd in the process.
-// Changes the str_status field to "RUN" using snprintf().  Creates a
-// pipe for out_pipe to capture standard output.  In the parent
-// process, ensures that the pid field is set to the child PID. In the
-// child process, directs standard output to the pipe using the dup2()
-// command. For both parent and child, ensures that unused file
-// descriptors for the pipe are closed 
+    
 }
 void cmd_update_state(cmd_t *cmd, int block){
 
@@ -130,22 +114,6 @@ void cmd_update_state(cmd_t *cmd, int block){
         }                                                                         
     }                        
 }                          
-// If the finished flag is 1, does nothing. Otherwise, updates the
-// state of cmd.  Uses waitpid() and the pid field of command to wait
-// selectively for the given process. Passes block (one of DOBLOCK or
-// NOBLOCK) to waitpid() to cause either non-blocking or blocking
-// waits.  Uses the macro WIFEXITED to check the returned status for
-// whether the command has exited. If so, sets the finished field to 1
-// and sets the cmd->status field to the exit status of the cmd using
-// the WEXITSTATUS macro. Calls cmd_fetch_output() to fill up the
-// output buffer for later printing.
-//
-// When a command finishes (the first time), prints a status update
-// message of the form
-//
-// @!!! ls[#17331]: EXIT(0)
-//
-// which includes the command name, PID, and exit status.
 
 char *read_all(int fd, int *nread){
     int max_size = BUFSIZE, cur_pos = 0;         // initial max and position
@@ -153,15 +121,17 @@ char *read_all(int fd, int *nread){
     int bytes_read = 1;
     
  do {  
-    bytes_read = read(fd, buf+cur_pos, max_size-cur_pos);    //we need to keep track of our place in buffer but idk how
-   if(bytes_read > 0){
-        cur_pos+=bytes_read;                //want to keep track of our current position in the buffer 
-   }   
-                                            // update current input
-    if(cur_pos == max_size){                       // check if more space is needed
+    bytes_read = read(fd, buf+cur_pos, max_size);//-cur_pos    
+  
+    cur_pos+=bytes_read;                // Keep track of our current position in the buffer 
+      
+    printf("max size: %d\n", max_size); 
+    printf("cur pos: %d \n", cur_pos); 
+    printf("bytes read: %d\n", bytes_read);                   // update current input
+    if(cur_pos >= max_size){  
+                            // check if more space is needed
         max_size *= 2;                               // double size of buffer
         char *newbuf = realloc(buf, max_size+1);     // pointer to either new or old location re-allocate, copies characters to new space if needed
-
         if(!newbuf){                            // check that re-allocation succeeded
             printf("ERROR: reallocation failed\n");    // if not...
             exit(1);                                  
@@ -174,15 +144,7 @@ char *read_all(int fd, int *nread){
     buf[cur_pos] = '\0';       
 return buf;   
  
-// Reads all input from the open file descriptor fd. Assumes
-// character/text output and null-terminates the character output with
-// a '\0' character allowing for printf() to print it later. Stores
-// the results in a dynamically allocated buffer which may need to
-// grow as more data is read.  Uses an efficient growth scheme such as
-// doubling the size of the buffer when additional space is
-// needed. Uses realloc() for resizing.   Ensures the return
-// string is null-terminated. Does not call close() on the fd as this
-// is done elsewhere.
+
 }
 void cmd_fetch_output(cmd_t *cmd){
     int bytes_read = 0;
@@ -202,18 +164,10 @@ void cmd_fetch_output(cmd_t *cmd){
     }
    
 }
-// If cmd->finished is zero, prints an error message with the format
-// 
-// ls[#12341] not finished yet
-// 
-// Otherwise retrieves output from the cmd->out_pipe and fills
-// cmd->output setting cmd->output_size to number of bytes in
-// output. Makes use of read_all() to efficiently capture
-// output. Closes the pipe associated with the command after reading
-// all input.
+
 
 void cmd_print_output(cmd_t *cmd){ 
-    //int *bytes_written = -1;
+    
     if(cmd->output == NULL) {
         printf("%s[#%d] : output not ready\n", (cmd->name), (cmd->pid));
     }
@@ -223,8 +177,6 @@ void cmd_print_output(cmd_t *cmd){
         printf("%p\n",(cmd->output));
 
     }
-    //close(cmd->outpipe[PWRITE]);
 
 }
-// Prints the output of the cmd contained in the output field if it is
-// non-null. 
+
--- cmdcol.c
+++ cmdcol.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h> //don't know if we need this
+#include <stdlib.h> 
 #include "commando.h"
 
 void cmdcol_add(cmdcol_t *col, cmd_t *cmd){
-    col->cmd[col->size] = cmd;
-    col->size++;
+    
+    col->cmd[col->size] = cmd; // Add cmd to the cmdcol struct
+    col->size++; // Increment cmdcol size
 
-    if (col->size > MAX_CMDS){ //can we do this after? or should it be before
+    if (col->size > MAX_CMDS){ 
         printf("Error, max commands exceeded");
     }
     
 }
-// Add the given cmd to the col structure. Update the cmd[] array and
-// size field. Report an error if adding would cause size to exceed
-// MAX_CMDS, the maximum number commands supported.
+
 
 void cmdcol_print(cmdcol_t *col){
-// Print all cmd elements in the given col structure.  The format of
-// the table is
 
-    printf("%-4s ", "JOB");
+
+    printf("%-4s ", "JOB");     // Format printing of job list
     printf("#%-8s ", "PID");
     printf("%4s ", "STAT");
     printf("%10s ", "STR_STAT");
     printf("%4s ", "OUTB");
     printf("%-s\n", "COMMAND");
 
-    for(int i = 0; i < col->size; i++) {
+    for(int i = 0; i < col->size; i++) {    // Iterate through job list and prints   
         printf("%-4d ", i);
         printf("#%-8d ", (col->cmd[i])->pid);
         printf("%4d ", (col->cmd[i])->status);
@@ -41,43 +39,22 @@ void cmdcol_print(cmdcol_t *col){
                 printf("\n");
                 break;
             }
-        //printf("\n");
+        
         }
     }
 }
-// JOB  #PID      STAT   STR_STAT OUTB COMMAND
-// 0    #17434       0    EXIT(0) 2239 ls -l -a -F 
-// 1    #17435       0    EXIT(0) 3936 gcc --help 
-// 2    #17436      -1        RUN   -1 sleep 2 
-// 3    #17437       0    EXIT(0)  921 cat Makefile 
-// 
-// Columns correspond to fields in the following way:
-// JOB:      index in the cmdcol_t struct
-// PID:      pid from the cmd_t struct
-// STAT:     status from the cmd_t struct
-// STR_STAT: str_status field from cmd_t
-// OUTB:     output_size from the cmd_t struct
-// COMMAND:  The contents of cmd->argv[] with a space
-//           between each element of the array.
-// 
-// Widths of the fields and justification are as follows
-// 
-// JOB  #PID      STAT   STR_STAT OUTB COMMAND
-// 1234 #12345678 1234 1234567890 1234 Remaining
-// left  left    right      right rigt left
-// int   int       int     string  int string
 
-void cmdcol_update_state(cmdcol_t *col, int block){
+
+void cmdcol_update_state(cmdcol_t *col, int block){ // Updates state of each cmd according to input block status
     for (int i = 0; i < col->size; i++) {
         cmd_update_state(col->cmd[i], block); 
     }
 }
-// Update each cmd in col by calling cmd_update_state() which is also
-// passed the block argument (either NOBLOCK or DOBLOCK) 
 
-void cmdcol_freeall(cmdcol_t *col){
+
+void cmdcol_freeall(cmdcol_t *col){ 
     for (int i = 0; i < col->size; i++) {
-        cmd_free(col->cmd[i]);
+        cmd_free(col->cmd[i]); // Frees each cmd in col
     }
 }
-// Call cmd_free() on all of the constituent cmd_t's.
+
